487956fa394c1a7103d48cbe67e44b4ae0ee9d0b
ahmsalah <aemdsalah@gmail.com>
2021-03-17 23:53:05 +0200

    Add containers directory

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,8 @@
-import Layout from '@/components/Layout/Layout';
-import PostItem from '@/components/PostItem/PostItem';
-import List from '@material-ui/core/List';
-import Paper from '@material-ui/core/Paper';
+import Posts from '@/containers/Posts/Posts';
 import axios from 'axios';
-import Head from 'next/head';
 
-export default function Home({ list }) {
-  console.log(list);
-  return (
-    <>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Layout>
-        <Paper>
-          <List>
-            {list.map((post, i) => (
-              <PostItem {...post} key={post.id} divider={i < list.length - 1} />
-            ))}
-          </List>
-        </Paper>
-      </Layout>
-    </>
-  );
+export default function Home(props) {
+  return <Posts {...props} />;
 }
 
 export async function getStaticProps(context) {
