432186ad352f85f4e460efa36bcb0e69e0558267
ahmsalah <aemdsalah@gmail.com>
2021-03-18 16:29:20 +0200

    Add Pagination to Posts

--- src/containers/Posts/Posts.js
+++ src/containers/Posts/Posts.js
@@ -1,10 +1,18 @@
 import List from '@material-ui/core/List';
+import Pagination from '@material-ui/core/Pagination';
 import Paper from '@material-ui/core/Paper';
+import { useRouter } from 'next/router';
 
 import Layout from '@/components/Layout/Layout';
 import PostItem from '@/components/PostItem/PostItem';
 
-export default function Posts({ list }) {
+export default function Posts({ list, pagesCount, page }) {
+  const router = useRouter();
+
+  const handleChange = (evt, value) => {
+    router.push(`/posts/${value}`);
+  };
+
   return (
     <Layout>
       <Paper>
@@ -13,6 +21,12 @@ export default function Posts({ list }) {
             <PostItem {...post} key={post.id} divider={i < list.length - 1} />
           ))}
         </List>
+        <Pagination
+          sx={{ padding: 3, display: 'flex', justifyContent: 'center' }}
+          onChange={handleChange}
+          page={+page}
+          count={pagesCount}
+        />
       </Paper>
     </Layout>
   );
