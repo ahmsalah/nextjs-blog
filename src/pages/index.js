eaf9b263fef1a55262f16f0d3335cc38b78c8cf8
ahmsalah <aemdsalah@gmail.com>
2021-03-18 12:46:16 +0200

    Update eslint config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
-import axios from 'axios';
-
+import API from '@/config/axios';
 import Posts from '@/containers/Posts/Posts';
 
 export default function Home(props) {
@@ -9,8 +8,8 @@ export default function Home(props) {
 export async function getStaticProps() {
   const postsPerPage = 9;
   const page = 1;
-  const { data: list } = await axios.get(
-    `https://blog-jsonserver.herokuapp.com/posts?_sort=id&_order=desc&_page=${page}&_limit=${postsPerPage}`,
+  const { data: list } = await API.get(
+    `/posts?_sort=id&_order=desc&_page=${page}&_limit=${postsPerPage}`,
   );
   return {
     props: { list }, // will be passed to the page component as props
