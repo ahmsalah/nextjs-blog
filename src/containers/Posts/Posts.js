65b8b75098f60eb73bac13d92f63b17bdfe8cfd7
ahmsalah <aemdsalah@gmail.com>
2021-03-18 11:52:18 +0200

    Remove Head tag from Posts

--- src/containers/Posts/Posts.js
+++ src/containers/Posts/Posts.js
@@ -2,25 +2,17 @@ import Layout from '@/components/Layout/Layout';
 import PostItem from '@/components/PostItem/PostItem';
 import List from '@material-ui/core/List';
 import Paper from '@material-ui/core/Paper';
-import Head from 'next/head';
 
 export default function Posts({ list }) {
-  console.log(list);
   return (
-    <>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Layout>
-        <Paper>
-          <List>
-            {list?.map((post, i) => (
-              <PostItem {...post} key={post.id} divider={i < list.length - 1} />
-            ))}
-          </List>
-        </Paper>
-      </Layout>
-    </>
+    <Layout>
+      <Paper>
+        <List>
+          {list?.map((post, i) => (
+            <PostItem {...post} key={post.id} divider={i < list.length - 1} />
+          ))}
+        </List>
+      </Paper>
+    </Layout>
   );
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -19,6 +19,7 @@ export default function MyApp({ Component, pageProps }) {
       <Head>
         <title>My page</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <ThemeProvider theme={theme}>
         <CssBaseline />
