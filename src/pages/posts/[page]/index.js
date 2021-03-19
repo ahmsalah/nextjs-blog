7fd5825b07f6fb75b0e09109b2fa82b8b39bdbeb
ahmsalah <aemdsalah@gmail.com>
2021-03-19 23:53:53 +0200

    Fetch data at request time in /posts

--- src/pages/posts/[page]/index.js
+++ src/pages/posts/[page]/index.js
@@ -8,23 +8,7 @@ export default function Home(props) {
 
 const getPagesCount = count => Math.ceil(count / postsPerPage, 10);
 
-export async function getStaticPaths() {
-  const {
-    headers: { ['x-total-count']: count },
-  } = await API.get(`/posts?_sort=id&_order=desc&_page=1&_limit=${postsPerPage}`);
-  const pagesCount = getPagesCount(count);
-
-  const paths = [...Array(pagesCount)].map((v, i) => ({
-    params: { page: `${i + 1}` },
-  }));
-
-  return {
-    paths,
-    fallback: false,
-  };
-}
-
-export async function getStaticProps({ params: { page } }) {
+export async function getServerSideProps({ params: { page } }) {
   const {
     data: list,
     headers: { ['x-total-count']: count },
