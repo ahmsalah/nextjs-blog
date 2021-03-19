a427481dbb361866e06bca3ea6885a25d1c4825e
ahmsalah <aemdsalah@gmail.com>
2021-03-19 23:04:41 +0200

    Add LinearProgress to Layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,33 @@
 import Box from '@material-ui/core/Box';
-import React, { memo } from 'react';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import { useRouter } from 'next/router';
+import React, { memo, useEffect, useState } from 'react';
 
 function Layout({ children }) {
+  const router = useRouter();
+  const [pageLoading, setPageLoading] = useState(false);
+
+  useEffect(() => {
+    const handleStart = () => {
+      setPageLoading(true);
+    };
+    const handleComplete = () => {
+      setPageLoading(false);
+    };
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
+  }, [router]);
   return (
-    <Box component="div">
+    <Box sx={{ position: 'relative' }}>
       {/* <Header /> */}
+      {pageLoading && <LinearProgress sx={{ position: 'absolute', top: 0, left: 0, right: 0 }} />}
       <Box
-        component="div"
         sx={{
-          py: 4,
+          py: 8,
           px: 3,
           maxWidth: 90 * 8,
-          my: 4,
           mx: 'auto',
         }}
       >
