2b31b816a9522015ff7133afa94160544b84f903
ahmsalah <aemdsalah@gmail.com>
2021-03-20 23:00:30 +0200

    Improve accessibility

--- src/components/PostItem/PostItem.js
+++ src/components/PostItem/PostItem.js
@@ -1,6 +1,7 @@
 import Collapse from '@material-ui/core/Collapse';
 import IconButton from '@material-ui/core/IconButton';
 import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
@@ -16,19 +17,35 @@ function PostItem({ title, body, id, divider, deletePost, editPost }) {
   return (
     <div>
       <Collapse in={!isEditing}>
-        <ListItem divider={divider}>
-          <Link href={`/post/${id}`} passHref>
-            <ListItemText primary={title} secondary={body} sx={{ cursor: 'pointer' }} />
-          </Link>
-          <IconButton onClick={() => deletePost(id)} edge="end" aria-label="delete">
-            <DeleteIcon />
-          </IconButton>
-          <IconButton onClick={() => setIsEditing(true)} edge="end" aria-label="edit">
-            <EditIcon />
-          </IconButton>
-        </ListItem>
+        <Link href={`/post/${id}`} passHref>
+          <ListItem divider={divider} button sx={{ paddingRight: 11 }}>
+            <ListItemText primary={title} secondary={body} />
+            <ListItemSecondaryAction>
+              <IconButton
+                onClick={e => {
+                  e.stopPropagation();
+                  deletePost(id);
+                }}
+                edge="end"
+                aria-label="delete"
+              >
+                <DeleteIcon />
+              </IconButton>
+              <IconButton
+                onClick={e => {
+                  e.stopPropagation();
+                  setIsEditing(true);
+                }}
+                edge="end"
+                aria-label="edit"
+              >
+                <EditIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        </Link>
       </Collapse>
-      <Collapse in={isEditing}>
+      <Collapse in={isEditing} mountOnEnter unmountOnExit>
         <ListItem sx={{ p: 0 }}>
           <PostForm
             editPostId={id}
